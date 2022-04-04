1b36fb62abe56fddfad205a132597598f0fe12a5
profkmcleod <102765341+profkmcleod@users.noreply.github.com>
2022-04-04 11:45:01 -0400

    Add files via upload

---
+++ scripts/my-first-js.js
@@ -0,0 +1,25 @@
+/* ---------------Challenge #1 -------------------- */
+
+// Set a variable equal to your name.
+
+// Request that the user inputs your name.
+
+/* If the name entered equals your name, display a message that reads,
+'You guessed it' (or some correct message of your choice) */
+
+/* If the name entered does not equal your name, display a message informing them
+that the guess was incorrect. */
+
+
+/*-----------------Challenge #2 ------------------*/
+
+/* Create a loop that will display the content of an array. 
+to the console. */
+
+
+/*----------------Challenge #3 ------------------- */
+
+//Request that the user enter their name.
+
+// After the user enter's their name, update the h1 on the page
+// to read "Welcome to my Site, [name]."
